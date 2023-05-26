526754639ee491c817834e7c7b508fdf7abd5cc7
polyakovdi <goodboy393393@gmail.com>
2023-05-26 16:33:05 +0300

    fix

--- js/createRequest.js
+++ js/createRequest.js
@@ -1,26 +1,15 @@
-const createRequest = (options = {}) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open(options.method || "POST", options.url, true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.send(options.params);
-  xhr.onreadystatechange = () => {
-		if (xhr.readyState === 4) {
-			if (xhr.status === 200) {
-				try {
-					const response = JSON.parse(xhr.responseText);
-					for (let key in response) {
-						if (response[key].err) {
-							alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
-							return;
-						}
-					}
-					options.callback(response);
-				} catch (err) {
-					alert(`Ошибка ответа: ${err.message}`);
-				}
-			} else {
-				alert(`Ошибка запроса: ${xhr.status} ${xhr.statusText}`);
-			}
-		}
-	};
-};
+function createRequest(argument, func) {
+    const xhr = new XMLHttpRequest();
+  
+    xhr.addEventListener('load', () => {
+      const response = JSON.parse(xhr.responseText);
+      
+      func(response);
+    });
+  
+    xhr.open('POST', 'https://jscp-diplom.netoserver.ru/', true);
+  
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  
+    xhr.send(argument);
+  }
